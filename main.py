19350a0066f1976987dc2f581adeb437821b68d1
Justin Zimmer <jtzimmer6@gmail.com>
2017-03-17 18:07:18 -0700

    more shit

--- main.py
+++ main.py
@@ -3,11 +3,15 @@
 from sys import argv
 from os import path
 from mod import config
+from mod import call
+from mod import state
 from mod.dal import dialstatus
 from mod.dal import carriers
 from mod.util import asterisk
 from mod.util import phone
 from mod.util import logger
+from multiprocessing import Process
+from functools import partial
 
 """
 Scan List of Numbers, lookup carrier, create Call File
@@ -25,3 +29,97 @@ def arguments():
         arguments = argv[1]
         print arguments
         return arguments
+
+def main():
+    """ Primary Campaign Run Script """
+    
+    calls = {}
+    number = "4803326545"
+    parsed_number = parse_phone(number)
+    calls[parsed_number.e164] = Call(parsed_number)
+
+    number_file, action, config_file = arguments()
+    if not path.exists(numberFile) or stat(numberFile).st_size == 0:
+        print """Specified file does not exist or is empty,
+please check filename and try again!"""
+        exit(1)
+    cfgDict = {}
+    if path.exists(configfile):
+        try:
+            cfgDict = json.load(open(configfile))
+        except:
+            print "{0} is invalid!\n".format(configfile)
+            exit(1)
+    else:
+        makeNew = raw_input("""You are missing the necessary parameter file: {0}.\n
+            Would you like to create a new one and manually enter parameters?
+            (Y/N): """.format(configfile))
+        if makeNew.strip()[0:1].upper() == 'Y':
+            cfgDict = cfgParams(configfile)
+        else:
+            print "Oh well, I tried..."
+            exit(1)
+    maxconcurrent = int(cfgDict['maxconcurrent'])
+    cps = float(cfgDict['cps'])
+    batch = []
+    interval = 1/cps
+    partial_testpool = partial(
+        testpool,
+        ani=cfgDict['testani'],
+        config=configfile,
+        cps=cps
+        )
+    partial_makecall = partial(
+        makecall,
+        ani=cfgDict['testani'],
+        accountsid=cfgDict['accountsid'],
+        authtoken=cfgDict['authtoken']
+        )
+    with open(numberFile) as f:
+        for line in f:
+            number = line.strip()
+            if not number:
+                print "Line is empty... what gives? Skipping..."
+                continue
+ 
+            if action.lower() == 'call':
+                print "Calling {0}".format(number)
+                process = Process(target=partial_makecall, args=(number,))
+                process.start()
+                batch.append(process)
+                print "{0} has been pooled.".format(number)
+                time.sleep(interval)
+                if len(batch) >= maxconcurrent:
+                    batch = cleanbatch(batch)
+            elif action.lower() == 'test':
+                process = Process(target=partial_testpool, args=(number,))
+                process.start()
+                batch.append(process)
+                time.sleep(interval)
+                if len(batch) >= maxconcurrent:
+                    batch = cleanbatch(batch)
+            else:
+                print """You did not specify a valid action for this list.\n
+                    Valid actions are: import, remove, enable, disable."""
+                exit(1)
+        while len(batch) > 0:
+            print "Emptying batch"
+            batch = cleanbatch(batch)
+        print "Action: {0} for file: {1} has been completed!".format(
+            action, numberFile)
+
+
+def cleanbatch(batch):
+    for proc in batch:
+        proc.join()
+        if not proc.is_alive():
+            proc.terminate()
+            batch.remove(proc)
+            print "Batch now has {0} Processes.".format(
+                len(batch)
+            )
+    return batch
+
+
+if __name__ == '__main__':
+    main()
--- mod/util/phone.py
+++ mod/util/phone.py
@@ -1,21 +1,53 @@
 
+import phonenumbers
 from twilio.rest.lookups import TwilioLookupsClient
 """
 Utilities for managing phone number information and formatting.
+
+API returns:
+
+{
+    "url": "https://lookups.twilio.com/v1/PhoneNumbers/+15108675309?Type=carrier",
+    "carrier": {
+        "error_code": null,
+        "type": "mobile",
+        "name": "T-Mobile USA, Inc.",
+        "mobile_network_code": "160",
+        "mobile_country_code": "310"
+    },
+    "national_format": "(510) 867-5309",
+    "phone_number": "+15108675309",
+    "country_code": "US"
+}
 
 """
 __author__ = 'Justin Zimmer'
 
 
-def lookup_number(number, account_sid, auth_token):
+def parse_phone(number, region="US"):
+    parsed_number = phonenumbers.parse(number, region)
+    parsed_number.e164 = format_e164(parsed_number)
+
+def format_e164(parsed_number):
+    """ Parse and return e.164 phone number string. """
+    return phonenumbers.format_number(parsed_number,
+        phonenumbers.PhoneNumberFormat.E164)
+
+def lookup_number(parsed_number, main_config):
     """Lookup carrier information for the specified phone number."""
 
-    client = TwilioLookupsClient(account_sid, auth_token)
+    client = TwilioLookupsClient(main_config["twilio_sid"], main_config["twilio_token"])
     try:
-        response = client.phone_numbers.get(number, include_carrier_info=True)
-        return response.carrier
+        response = client.phone_numbers.get(parsed_number.e164, include_carrier_info=True)
+        parsed_number.carrier = response.carrier["name"]
+        parsed_number.type = response.carrier["type"]
+        return parsed_number
     except TwilioRestException as error:
         if error.code == 20404:
-            return False
+            parsed_number.carrier = "None"
+            parsed_number.type = "invalid"
+            return parsed_number
         else:
-            raise error
+            parsed_number.carrier = error.message
+            parsed_number.type = error.code
+            return parsed_number
