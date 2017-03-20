8c5deb9d63e847ba0751fb0b51e27cd02eb9a40e
Justin Zimmer <jtzimmer6@gmail.com>
2017-03-20 16:15:39 -0700

    more DAL

--- mod/dal.py
+++ mod/dal.py
@@ -9,7 +9,7 @@ class DAL(object):
     def __init__(self, config):
         self.db_path = config["db_path"]
         self.db_type = config["db_type"]
-        self.db = __import__(self.db_type)
+        self.db_mod = __import__(self.db_type)
 
     def table_exists(self, table_name):
         """
@@ -19,7 +19,7 @@ class DAL(object):
 
         """
 
-        conn = self.db.connect(self.db_path) # :memory: is an interesting option
+        conn = self.db_mod.connect(self.db_path) # :memory: is an interesting option
         cur = conn.cursor()
         cmd = "SELECT name FROM sqlite_master WHERE type='table' AND name='{0}';".format(table_name)
         # If the carriers table already exists, drop and reload
@@ -28,5 +28,90 @@ class DAL(object):
         else:
             return True
         conn.close()
+
+    def drop_table(self, table_name):
+        """
+        Drop specified table.
+
+        -- Doctest --
+
+        """
+
+        conn = self.db_mod.connect(self.db_path) # :memory: is an interesting option
+        cur = conn.cursor()
+        cmd = "DROP TABLE IF EXISTS '{0}';".format(table_name)
+        # If the carriers table already exists, drop and reload
+        cur.execute(cmd)
+        conn.commit()
+        conn.close()
+
+    def get_first(self, cmd, params):
+        """
+        Execute cmd and return all results.
+
+        -- Doctest --
+
+        """
+        conn = self.db_mod.connect(self.db_path) # :memory: is an interesting option
+        cur = conn.cursor()
+        cur.execute(cmd, params)
+        result = cur.fetchone()
+        conn.close()
+        return results
+
+    def get_all(self, cmd, params):
+        """
+        Execute cmd and return all results.
+
+        -- Doctest --
+
+        """
+        conn = self.db_mod.connect(self.db_path) # :memory: is an interesting option
+        cur = conn.cursor()
+        cur.execute(cmd, params)
+        results = cur.fetchall()
+        conn.close()
+        return results
+
+    def insert_rows(self, cmd, values):
+        """
+        Execute cmd and return all results.
+
+        -- Doctest --
+
+        """
+
+        conn = self.db_mod.connect(self.db_path) # :memory: is an interesting option
+        cur = conn.cursor()
+
+        if isinstance(values, list):
+            cur.executemany(cmd, values)
+        else:
+            cur.execute(cmd, values)
+
+        conn.commit()
+        conn.close()
+        return True
+
+    def create_table(self, table_name, fields):
+        """
+        Create table with fields.
+
+        --- Doctest --
+        """
+
+        conn = self.db_mod.connect(self.db_path) # :memory: is an interesting option
+        cur = conn.cursor()
+        field_str = "("
+        for field in fields:
+            field_str = "{0}{1}, ".format(field_str, field)
+        field_str = field_str[:-2] + ")"
+        cmd = "CREATE TABLE IF NOT EXISTS {0} {1};".format(table_name, field_str)
+        cur.execute(cmd)
+        conn.close()
+
+
+
+
 
         
--- mod/dal/carriers.py
+++ mod/dal/carriers.py
@@ -1,5 +1,4 @@
 
-import sqlite3
 """ Handle carrier database """
 
 class Carriers(mod.dal.DAL):
@@ -19,19 +18,13 @@ class Carriers(mod.dal.DAL):
         """
         import csv
 
-        conn = sqlite3.connect(self.db_path) # :memory: is an interesting option
-        cur = conn.cursor()
+        if self.table_exists('carriers'):
+            self.drop_table('carriers')
 
-        # If the carriers table already exists, drop and reload
-        if not self.table_exists('carriers'):
-            cur.execute(
-                "CREATE TABLE carriers (carrierName, accessNumber);")
-            with open(carrier_file, 'rb') as csv_file:
-                # csv.DictReader uses first line in file for column headings by default
-                line = csv.DictReader(csv_file) # comma is default delimiter
-                to_db = [(row['carrierName'], row['accessNumber']) for row in line]
-            cur.executemany(
-                "INSERT INTO carriers (carrierName, accessNumber) VALUES (?, ?);", to_db)
-            conn.commit()
-        conn.close()
+        self.create_table(('carrierName', 'accessNumber'))
+        with open(carrier_file, 'rb') as csv_file:
+            # csv.DictReader uses first line in file for column headings by default
+            line = csv.DictReader(csv_file) # comma is default delimiter
+            to_db = [(row['carrierName'], row['accessNumber']) for row in line]
+        self.insert_rows(to_db)
         return True
